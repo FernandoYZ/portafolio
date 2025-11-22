74a81b8458db787266b0e945a85f20afc91c99a2
FernandoYZ <fernandozavala266@gmail.com>
2025-11-22 12:27:53 -0500

    feat(astro): agregar configuración personalizada

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,13 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },
+    server: {
+      host: true
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 3100,
+      allowedHosts: ['fernandoyz.com', 'www.fernandoyz.com', 'localhost']
+    }
+  }
 });
